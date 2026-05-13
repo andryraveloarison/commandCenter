d4f24900653f8221fe2812a008372bf87b3b48cc
andry <andryraveloarison01@gmail.com>
2026-05-13 15:38:38 +0300

    update security back

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
+    cors: true,
     watch: {
       usePolling: true,
       interval: 500,
